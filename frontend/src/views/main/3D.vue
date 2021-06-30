cc5f944e009d0d4e2e35a397b6d6587a12c69745
Yaroslav Pisarev <yaricp@gmail.com>
2021-07-01 00:57:10 +0700

    added some babylon components with light

--- frontend/src/views/main/3D.vue
+++ frontend/src/views/main/3D.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container fluid>
     <Scene>
-      <Box
-              :position="position"
-              :scaling="[size, size, size]"
-      ></Box>
-    </Scene>
+  <Camera type="arcRotate"></Camera>
+  <Entity>
+    <Animation property="rotation.y" :duration="5" :end="Math.PI*2"></Animation>
+    <DirectionalLight specular="#0F0" diffuse="F00" :direction="[2,0,100]"></DirectionalLight>
+  </Entity>
+  <Box :position="[-2,0,0]">
+    <Material diffuse="#ffffff"></Material>
+  </Box>
+  <IcoSphere :position="[1.3,0,0]">
+    <Material diffuse="#FF0"></Material>
+    <Animation property="rotation.x" :duration="3" :end="Math.PI * 2"></Animation>
+    <Animation property="rotation.y" :duration="3" :end="Math.PI * 2"></Animation>
+  </IcoSphere>
+</Scene>
     <v-divider></v-divider>
     <v-text-field
       v-model="size"
@@ -28,10 +37,30 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { Entity, Scene, Box } from 'vue-babylonjs';
+import {
+  Entity,
+  Scene,
+  Box,
+  DirectionalLight,
+  Animation,
+  PointLight,
+  IcoSphere,
+  Camera,
+  Material,
+} from 'vue-babylonjs';
 @Component ({
   mixins: [Entity],
-  components: { Scene, Box },
+  components: {
+    Entity,
+    Scene,
+    Box,
+    DirectionalLight,
+    Animation,
+    PointLight,
+    IcoSphere,
+    Camera,
+    Material,
+  },
 })
 export default class Dashboard extends Vue {
     public file = '';
